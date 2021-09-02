3599521c201096486caa43bf55c19ec7b272bfa1
???? ??? <you@vevo.uz>
2021-09-02 19:58:40 +0500

    google ads

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -44,12 +44,14 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Eventually' },
-                { name: 'keywords', content: 'Kirill, Lotin' },
+                { name: 'description', content: "Kirill lotin konverter, ushbu servis hujjatlaringizni lotin alifbosiga o'tkazishga yordam beradi" },
+                { name: 'keywords', content: 'Kirill, Lotin, кирилл, лотин, латин' },
               ]}
             >
               <html lang="en" />
             </Helmet>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1605687623825790"
+              crossorigin="anonymous"></script>
             <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
               {children}
             </div>
