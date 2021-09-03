b7d06e710eda0f7b5dbf80c5ebd3b034c5407951
???? ??? <you@vevo.uz>
2021-09-03 11:34:40 +0500

    analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,28 +7,28 @@
 // You can delete this file if you're not using it
 
 // gatsby-ssr.js
-import React from 'react'
-import { oneLine } from 'common-tags'
+const React = require('react')
+const { oneLine } = require('common-tags')
 
-exports.onRenderBody = function ({ setHeadComponents }) {
-  const GST_ID = 'G-DY64YBCP1E'
-  if (process.env.NODE_ENV === `production`) {
-    setHeadComponents([
-      <script
-        key="google-gst-js"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GST_ID}`}
-      />,
-      <script
-        key="google-gst-init"
-        dangerouslySetInnerHTML={{
-          __html: oneLine`
+exports.onRenderBody = ({ setHeadComponents }) => {
+    const GST_ID = 'G-DY64YBCP1E'
+    if (process.env.NODE_ENV === `production`) {
+        setHeadComponents([
+            <script
+                key="google-gst-js"
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GST_ID}`}
+            />,
+            <script
+                key="google-gst-init"
+                dangerouslySetInnerHTML={{
+                    __html: oneLine`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GST_ID}');`,
-        }}
-      />,
-    ])
-  }
+                }}
+            />,
+        ])
+    }
 }
