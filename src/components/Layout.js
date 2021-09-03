98e2b55685e19fab283aa3de9152dd56633f2c0b
???? ??? <you@vevo.uz>
2021-09-03 10:22:47 +0500

    analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -52,7 +52,7 @@ class Layout extends Component {
               <script async src="https://www.googletagmanager.com/gtag/js?id=G-DY64YBCP1E"></script>
               <script>
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments)}
+                function gtag(){window.dataLayer.push(arguments)}
                 gtag('js', new Date());
 
                 gtag('config', 'G-DY64YBCP1E');
