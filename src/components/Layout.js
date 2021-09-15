18902d46c685868221e18ad3ccdada32245f0e34
???? ??? <you@vevo.uz>
2021-09-16 03:14:49 +0500

    gads

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -56,10 +56,13 @@ class Layout extends Component {
               ]}
             >
               <html lang="en" />
-              <script async src="https://www.googletagmanager.com/gtag/js?id=G-DY64YBCP1E"></script>
+              {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-DY64YBCP1E"></script> */}
             </Helmet>
-            {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1605687623825790"
-              crossorigin="anonymous"></script> */}
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1605687623825790"
+              crossorigin="anonymous"></script>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+            </script>
+
             <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
               {children}
             </div>
